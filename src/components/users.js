35a58996a29e657b18e9558cec0630a8aceffa68
John Pelley <jryanp@yahoo.com>
2018-04-07 16:06:08 -0500

    John Pelley got it almost working but cors is not there

--- src/components/users.js
+++ src/components/users.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUsers } from '../actions';
 
+
 class Users extends Component {
   componentDidMount() {
     this.props.getUsers();
   }
-  
+
   render() {
     return (
       <ul>
         {this.props.users.map((user, i) => {
-          return <li key={i}>{user.email}</li>;
+          return <li key={i}>{user.username}</li>;
         })}
       </ul>
     );
